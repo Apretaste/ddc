dac7a87d77da27f0bf59bb0b4704e65ba9809d94
Gogs <gogs@fake.local>
2019-02-04 22:49:49 -0500

    update to ejs

--- service.php
+++ service.php
@@ -1,4 +1,5 @@
 <?php
+
 use Goutte\Client;
 
 class Service
@@ -19,23 +20,33 @@ class Service
 	 * Call to show the news
 	 *
 	 * @param Request
-	 * @return Response
+	 * @param Response
+	 *
+	 * @return void
 	 */
-	public function _buscar(Request $request)
+	public function _buscar(Request $request, Response &$response)
 	{
 		// no allow blank entries
-		if (empty($request->query))
+		if(empty($request->input->data->query))
 		{
 
 			$response->setLayout('diariodecuba.ejs');
-			$response->createFromText("Su busqueda parece estar en blanco, debe decirnos sobre que tema desea leer");
-			}
+			$response->setTemplate('text.ejs', [
+				"title" => "Su busqueda parece estar en blanco",
+				"body" => "debe decirnos sobre que tema desea leer"
+			]);
+			return;
+		}
 
 		// search by the query
-		try {
-			$articles = $this->searchArticles($request->query);
-		} catch (Exception $e) {
-			return $this->respondWithError();
+		try
+		{
+			$articles = $this->searchArticles($request->input->data->query);
+		} catch(Exception $e)
+		{
+			$this->respondWithError($response);
+
+			return;
 		}
 
 		// error if the search returns empty
@@ -43,13 +54,18 @@ class Service
 		{
 
 			$response->setLayout('diariodecuba.ejs');
-			$response->createFromText("Su busqueda <b>{$request->query}</b> no gener&oacute; ning&uacute;n resultado. Por favor cambie los t&eacute;rminos de b&uacute;squeda e intente nuevamente.");
-			}
+			$response->setTemplate("text.ejs", [
+				"title" => "Su busqueda parece estar en blanco",
+				"body" => "Su busqueda <b>{$request->input->data->query}</b> no gener&oacute; ning&uacute;n resultado. Por favor cambie los t&eacute;rminos de b&uacute;squeda e intente nuevamente."
+			]);
 
-		$responseContent = array(
+			return;
+		}
+
+		$responseContent = [
 			"articles" => $articles,
-			"search" => $request->query
-		);
+			"search" => $request->input->data->query
+		];
 
 		$response->setLayout('diariodecuba.ejs');
 		$response->setTemplate("searchArticles.ejs", $responseContent);
@@ -67,21 +83,33 @@ class Service
 		$link = $request->input->data->link;
 
 		// no allow blank entries
-		if (empty($link)) {
-			$response->createFromText("Su busqueda parece estar en blanco, debe decirnos que articulo quiere leer");
+		if(empty($link))
+		{
+			$response->setLayout('diariodecuba.ejs');
+			$response->setTemplate("text.ejs", [
+				"title" => "Su busqueda parece estar en blanco",
+				"body" => "Su busqueda parece estar en blanco, debe decirnos que articulo quiere leer"
+			]);
+
+			return;
 		}
 
 		// send the actual response
-		try {
+		try
+		{
 			$responseContent = $this->story($link);
-		} catch (Exception $e) {
-			return $this->respondWithError();
+		} catch(Exception $e)
+		{
+			$this->respondWithError($response);
+
+			return;
 		}
 
 		// get the image if exist
-		$images = array();
-		if( ! empty($responseContent['img'])) {
-			$images = array($responseContent['img']);
+		$images = [];
+		if( ! empty($responseContent['img']))
+		{
+			$images = [$responseContent['img']];
 		}
 
 		$response->setCache();
@@ -93,21 +121,28 @@ class Service
 	 * Call list by categoria
 	 *
 	 * @param Request
-	 * @return Response
+	 * @param Response
+	 *
+	 * @return void
 	 */
-	public function _categoria(Request $request)
+	public function _categoria(Request $request, Response &$response)
 	{
-		if (empty($request->query))
+		if(empty($request->input->data->query))
 		{
 
 			$response->setLayout('diariodecuba.ejs');
-			$response->createFromText("Su busqueda parece estar en blanco, debe decirnos sobre que categor&iacute;a desea leer");
-			}
+			$response->setTemplate("text.ejs", [
+				"title" => "Su busqueda parece estar en blanco",
+				"body" => "Su busqueda parece estar en blanco, debe decirnos sobre que categor&iacute;a desea leer"
+			]);
 
-		$responseContent = array(
-			"articles" => $this->listArticles($request->query)["articles"],
-			"category" => $request->query
-		);
+			return;
+		}
+
+		$responseContent = [
+			"articles" => $this->listArticles($request->input->data->query)["articles"],
+			"category" => $request->input->data->query
+		];
 
 		$response->setLayout('diariodecuba.ejs');
 		$response->setTemplate("catArticles.ejs", $responseContent);
@@ -117,24 +152,29 @@ class Service
 	 * Search stories
 	 *
 	 * @param String
-	 * @return Array
+	 *
+	 * @return array
 	 */
 	private function searchArticles($query)
 	{
 		// Setup crawler
 		$client = new Client();
-		$url = "http://www.diariodecuba.com/search/node/".urlencode($query);
+		$url = "http://www.diariodecuba.com/search/node/" . urlencode($query);
 		$crawler = $client->request('GET', $url);
 
 		// Collect saearch by term
-		$articles = array();
-		$crawler->filter('div.search-result')->each(function($item) use (&$articles){
+		$articles = [];
+		$crawler->filter('div.search-result')->each(function($item) use (&$articles)
+		{
 
-			$articles[] = ["description" => $item->filter('p.search-snippet')->text(),
-						   "title"	=> $item->filter('h1.search-title > a')->text(),
-						   "link" => str_replace('http://www.diariodecuba.com/',"",$item->filter('h1.search-title > a')->attr("href"))];
-						   
+			$articles[] = [
+				"description" => $item->filter('p.search-snippet')->text(),
+				"title" => $item->filter('h1.search-title > a')->text(),
+				"link" => str_replace('http://www.diariodecuba.com/', "", $item->filter('h1.search-title > a')->attr("href"))
+			];
+
 		});
+
 		return $articles;
 	}
 
@@ -142,7 +182,8 @@ class Service
 	 * Get the array of news by content
 	 *
 	 * @param String
-	 * @return Array
+	 *
+	 * @return array
 	 */
 	private function listArticles($query)
 	{
@@ -151,13 +192,13 @@ class Service
 		$crawler = $client->request('GET', "http://www.diariodecuba.com/rss.xml");
 
 		// Collect articles by category
-		$articles = array();
+		$articles = [];
 		$crawler->filter('channel item')->each(function($item, $i) use (&$articles, $query)
 		{
 			// if category matches, add to list of articles
 			$item->filter('category')->each(function($cat, $i) use (&$articles, $query, $item)
 			{
-				if (strtoupper($cat->text()) == strtoupper($query))
+				if(strtoupper($cat->text()) == strtoupper($query))
 				{
 					$title = $item->filter('title')->text();
 					$link = $this->urlSplit($item->filter('link')->text());
@@ -169,31 +210,31 @@ class Service
 					$description = preg_replace("/<\/?p[^>]*>/", '', $description);//quitamos las <p></p>
 
 					$author = "desconocido";
-					if ($item->filter('dc|creator')->count() > 0)
+					if($item->filter('dc|creator')->count() > 0)
 					{
 						$authorString = trim($item->filter('dc|creator')->text());
 						$author = "({$authorString})";
 					}
 
-					$articles[] = array(
+					$articles[] = [
 						"title" => $title,
 						"link" => $link,
 						"pubDate" => $pubDate,
 						"description" => $description,
 						"author" => $author
-					);
+					];
 				}
 			});
 		});
 
 		// Return response content
-		return array("articles" => $articles);
+		return ["articles" => $articles];
 	}
 
 	/**
 	 * Get all stories from a query
 	 *
-	 * @return Array
+	 * @return array
 	 */
 	private function allStories()
 	{
@@ -203,9 +244,9 @@ class Service
 		$client->setClient($guzzle);
 
 		// create a crawler
-		$crawler = $client->request('GET', "http://www.diariodecuba.com/rss.xml"); 
+		$crawler = $client->request('GET', "http://www.diariodecuba.com/rss.xml");
 
-		$articles = array();
+		$articles = [];
 		$crawler->filter('channel item')->each(function($item, $i) use (&$articles)
 		{
 
@@ -218,22 +259,25 @@ class Service
 			$description = preg_replace("/<\/?a[^>]*>/", '', $description);//quitamos las <a></a>
 			$description = preg_replace("/<\/?p[^>]*>/", '', $description);//quitamos las <p></p>
 			$pubDate = $item->filter('pubDate')->text();
-			$category = $item->filter('category')->each(function($category, $j) {return $category->text();});
+			$category = $item->filter('category')->each(function($category, $j){ return $category->text(); });
 
-			if ($item->filter('dc|creator')->count() == 0) $author = "desconocido";
+			if($item->filter('dc|creator')->count() == 0)
+			{
+				$author = "desconocido";
+			}
 			else
 			{
 				$authorString = trim($item->filter('dc|creator')->text());
 				$author = "({$authorString})";
 			}
 
-			$categoryLink = array();
-			foreach ($category as $currCategory)
+			$categoryLink = [];
+			foreach($category as $currCategory)
 			{
 				$categoryLink[] = $currCategory;
 			}
 
-			$articles[] = array(
+			$articles[] = [
 				"title" => $title,
 				"link" => $link,
 				"pubDate" => $pubDate,
@@ -241,18 +285,19 @@ class Service
 				"category" => $category,
 				"categoryLink" => $categoryLink,
 				"author" => $author
-			);
+			];
 		});
 
 		// return response content
-		return array("articles" => $articles);
+		return ["articles" => $articles];
 	}
 
 	/**
 	 * Get an specific news to display
 	 *
 	 * @param String
-	 * @return Array
+	 *
+	 * @return array
 	 */
 	private function story($query)
 	{
@@ -270,18 +315,20 @@ class Service
 		// get the intro
 
 		$titleObj = $crawler->filter('div.content:nth-child(1) p:nth-child(1)');
-		$intro = $titleObj->count()>0 ? $titleObj->text() : "";
+		$intro = $titleObj->count() > 0 ? $titleObj->text() : "";
 
 		// get the images
 		$imageObj = $crawler->filter('figure.field-field-image-content img');
-		$imgUrl = ""; $imgAlt = ""; $img = "";
-		if ($imageObj->count() != 0)
+		$imgUrl = "";
+		$imgAlt = "";
+		$img = "";
+		if($imageObj->count() != 0)
 		{
 			$imgUrl = trim($imageObj->attr("src"));
 			$imgAlt = trim($imageObj->attr("alt"));
 
 			// get the image
-			if ( ! empty($imgUrl))
+			if( ! empty($imgUrl))
 			{
 				$imgName = $this->utils->generateRandomHash() . "." . pathinfo($imgUrl, PATHINFO_EXTENSION);
 				$img = \Phalcon\DI\FactoryDefault::getDefault()->get('path')['root'] . "/temp/$imgName";
@@ -291,27 +338,28 @@ class Service
 
 		// get the array of paragraphs of the body
 		$paragraphs = $crawler->filter('div.node div.content p');
-		$content = array();
-		foreach ($paragraphs as $p)
+		$content = [];
+		foreach($paragraphs as $p)
 		{
 			$content[] = trim($p->textContent);
 		}
 
 		// create a json object to send to the template
-		return array(
+		return [
 			"title" => $title,
 			"intro" => $intro,
 			"img" => $img,
 			"imgAlt" => $imgAlt,
 			"content" => $content,
 			"url" => "http://www.diariodecuba.com/$query"
-		);
+		];
 	}
 
 	/**
 	 * Get the link to the news starting from the /content part
 	 *
 	 * @param String
+	 *
 	 * @return String
 	 * http://www.martinoticias.com/content/blah
 	 */
@@ -321,20 +369,28 @@ class Service
 		unset($url[0]);
 		unset($url[1]);
 		unset($url[2]);
+
 		return implode("/", $url);
 	}
 
 	/**
 	 * Return a generic error email, usually for try...catch blocks
 	 *
-	 * @auhor salvipascual
-	 * @return Respose
+	 * @author salvipascual
+	 * @author kumahacker
+	 *
+	 * @param Response
+	 *
+	 * @return void
 	 */
-	private function respondWithError()
+	private function respondWithError(Response &$response)
 	{
 		error_log("WARNING: ERROR ON SERVICE DIARIO DE CUBA");
 
 		$response->setLayout('diariodecuba.ejs');
-		$response->createFromText("Lo siento pero hemos tenido un error inesperado. Enviamos una peticion para corregirlo. Por favor intente nuevamente mas tarde.");
+		$response->setTemplate("text.ejs", [
+			"title" => "Error inesperado",
+			"body" => "Lo siento pero hemos tenido un error inesperado. Enviamos una peticion para corregirlo. Por favor intente nuevamente mas tarde.",
+		]);
 	}
 }
