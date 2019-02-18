8642aac1a6dd895eda3ac41a73bc7b43a2dbb216
Gogs <gogs@fake.local>
2019-02-18 12:44:58 -0500

    format date

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function formatDate(date) {
+  var monthNames = [
+    "Enero", "Febrero", "Marzo",
+    "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Septiembre", "Octubre",
+    "Noviembre", "Diciembre"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
