2fbfb46cc86a8a4dc23f60aa4f7f1b42f4f8ea53
Gogs <gogs@fake.local>
2019-08-13 20:42:09 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,27 +1,29 @@
-$(document).ready(function() {
-	// focus input when modal opens
-	$('.modal').modal({
-		onOpenEnd: function() {
-			$('#query').focus();
-		}
-	});
+$(document).ready(function () {
+  // focus input when modal opens
+  $('.modal').modal({
+    onOpenEnd: function () {
+      $('#query').focus();
+    }
+  });
 
-	// submit search on enter
-	$('#query').keypress(function (e) {
-		if (e.which == 13) {
-			sendSearch();
-			return false;
-		}
-	});
+  // submit search on enter
+  $('#query').keypress(function (e) {
+    if (e.which === 13) {
+      sendSearch();
+      return false;
+    }
+  });
 });
 
 function sendSearch() {
-	var query = $('#query').val().trim();
-	if(query.length >= 3) {
-		apretaste.send({
-			'command':'DIARIODECUBA BUSCAR',
-			'data':{query: query}
-		});
-	}
-	else M.toast({html: 'Inserte mínimo 3 caracteres'});
+  var query = $('#query').val().trim();
+  if (query.length >= 3) {
+    apretaste.send({
+      'command': 'DIARIODECUBA BUSCAR',
+      'data': {query: query}
+    });
+  }
+  else {
+    M.toast({html: 'Inserte mínimo 3 caracteres'});
+  }
 }
